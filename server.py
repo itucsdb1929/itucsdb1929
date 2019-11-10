72901485b53d2b245a36cdcbbcc2f0c253aaa1fb
Osman Karaketir <osmankaraketir@gmail.com>
2019-11-10 16:21:33 +0300

    yet another test

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "Testing commits? Still Running ?"
+    return "Testing commits? Still Running ? what about now?"
 
 
 if __name__ == "__main__":
