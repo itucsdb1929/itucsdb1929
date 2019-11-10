0cfafed9a7f40040c3e1a904af685bd1acc4cbc5
kaplanyb <kaplanbaris33@hotmail.com>
2019-11-10 17:29:32 +0300

    Add templates folder

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home_page():
     return "Testing commits? Still Running ? what about now?"
 
+@app.route('/<user>')
+def hello_name(user):
+    return render_template('base.html', name = user)
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host = '0.0.0.0', port = 5000, debug=True)
