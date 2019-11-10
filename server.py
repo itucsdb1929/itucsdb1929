94de0046332a65972068d3aed2d977118e17d5f2
Osman Karaketir <osmankaraketir@gmail.com>
2019-11-10 16:53:21 +0300

    development test

--- server.py
+++ server.py
@@ -1,12 +1,41 @@
 from flask import Flask
 
+import os
+import sys
+
+import psycopg2 as dbapi2
+
+
+INIT_STATEMENTS = [
+    "CREATE TABLE IF NOT EXISTS AnotherDUMMY (NUM INTEGER)",
+    "INSERT INTO DUMMY VALUES (50)",
+]
+
+
+def initialize(url):
+    with dbapi2.connect(url) as connection:
+        cursor = connection.cursor()
+        for statement in INIT_STATEMENTS:
+            cursor.execute(statement)
+        cursor.close()
+
+
+def testFonk:
+    url = os.getenv("DATABASE_URL")
+    if url is None:
+        print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
+        sys.exit(1)
+    initialize(url)
+
+
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home_page():
-    return "Testing commits? Still Running ? what about now?"
+    return "development test"
 
 
 if __name__ == "__main__":
