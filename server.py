783c0a47e5241e3bb21e0a91af539576fd402bec
Osman Karaketir <osmankaraketir@gmail.com>
2019-11-10 18:02:55 +0300

    first query

--- server.py
+++ server.py
@@ -6,26 +6,19 @@ import sys
 import psycopg2 as dbapi2
 
 
-INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS AnotherDUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (50)",
-]
-
-
 def initialize(url):
-    with dbapi2.connect(url) as connection:
-        cursor = connection.cursor()
-        for statement in INIT_STATEMENTS:
-            cursor.execute(statement)
-        cursor.close()
+    connection =  dbapi2.connect(url)
+    cursor = connection.cursor()
+    return (connection, cursor)
+    
 
 
-def testFonk:
+def testFonk():
     url = os.getenv("DATABASE_URL")
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
-    initialize(url)
+    return initialize(url)
 
 
 
@@ -35,7 +28,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "development test"
+    connection, cursor = testFonk()
+    cursor.execute("select * from dummy")
+    a = cursor.fetchall()    
+    returnStr = "deneme<br>"
+    for x in a:
+        for i in x:
+            returnStr += str(i)
+        returnStr +="<br>"
+    connection.close()
+    cursor.close()
+    return returnStr
 
 
 if __name__ == "__main__":
