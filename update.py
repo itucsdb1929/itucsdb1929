cae3e6a4c093d49f1d9c2d499c36f91e6354bb05
Osman Karaketir <osmankaraketir@gmail.com>
2019-11-11 21:48:51 +0300

    Threading test (#10)
    * multiThreading
    
    * thread commit

--- update.py
+++ update.py
@@ -3,11 +3,20 @@ import threading
 
 POOL_TIME = 5 #Seconds
 
-
+val = 1
 
 
 def update():
-    print('someGameStuffDone\n')
+    # print('someGameStuffDone\n')
+
+    cursor = db.get_cursor()
+    connection = db.get_connection()
+
+    cursor.execute("""INSERT INTO DUMMY VALUES (%s)""", (val,))
+
+    connection.commit()
+    global val
+    val = val + 1
     gameThread = threading.Timer(POOL_TIME, update, ())
     gameThread.start() 
 
