3f33aae2385486e98a1e7677cf143308b23af7f6
Osman Karaketir <osmankaraketir@gmail.com>
2019-12-15 19:38:31 +0300

    test (#46)

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 import db
 import threading
-
+import statements
 POOL_TIME = 5 #Seconds
 
 
@@ -11,6 +11,12 @@ def update():
     with db.dataBaseLock:
         cursor = db.get_cursor()
         connection = db.get_connection()
+
+        # all_users = statements.get_all_usernames(cursor)
+
+        # for a_user in all_users:
+        #     user_cities = statements.get_all_cities_of_user(cursor)
+        update_all_sources(cursor)
 
 
         connection.commit()
