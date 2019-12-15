90371b6505b3ab3b5b7a86c2f4c1f581684b09fb
kaplanyb <kaplanbaris33@hotmail.com>
2019-12-15 19:05:21 +0300

    Fix adminpanel git commit -m Fixadminpanel

--- adminpanel.py
+++ adminpanel.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, Blueprint, flash
 from statements import insert_city
 from dbinit import initialize
+from statements import INIT_STATEMENTS_ORDER, NEW_STATEMENTS
 import db, os
 
 
@@ -13,16 +14,17 @@ def adminpanel_func():
 
 @adminpanel.route("/adminpanel/addcity", methods=['POST'])
 def adminpanel_add_city():
-    cursor = db.get_cursor()
-    connection = db.get_connection()
-    cityname = request.form.get('cityname')
-    username = request.form.get('username')
-    xcoordinate = request.form.get('xcoordinate')
-    ycoordinate = request.form.get('ycoordinate')
-    buildinglimit = request.form.get('buildinglimit')
-    buildingcount = request.form.get('buildingcount')
-    insert_city(cursor, cityname, username, xcoordinate, ycoordinate, buildinglimit, buildingcount)
-    connection.commit()
+    with db.dataBaseLock:
+        cursor = db.get_cursor()
+        connection = db.get_connection()
+        cityname = request.form.get('cityname')
+        username = request.form.get('username')
+        xcoordinate = request.form.get('xcoordinate')
+        ycoordinate = request.form.get('ycoordinate')
+        buildinglimit = request.form.get('buildinglimit')
+        buildingcount = request.form.get('buildingcount')
+        insert_city(cursor, cityname, username, xcoordinate, ycoordinate, buildinglimit, buildingcount)
+        connection.commit()
     return redirect(url_for('adminpanel.adminpanel_func')) 
 
 @adminpanel.route("/adminpanel/addsql", methods=['POST'])
@@ -38,6 +40,10 @@ def adminpanel_insert_sql():
 
 @adminpanel.route("/adminpanel/dbinit", methods=['GET'])
 def adminpanel_db_init():
-    url = os.getenv("DATABASE_URL")
-    initialize(url)
+    with db.dataBaseLock:
+        cursor = db.get_cursor()
+        connection = db.get_connection()
+        for statement in INIT_STATEMENTS_ORDER:
+            cursor.execute(NEW_STATEMENTS[statement])
+        connection.commit()
     return redirect(url_for('adminpanel.adminpanel_func'))
