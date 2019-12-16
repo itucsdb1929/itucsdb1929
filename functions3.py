9e8fb5b0e66057f640295de22e3cc584dbd68294
kaplanyb <kaplanbaris33@hotmail.com>
2019-12-16 12:02:20 +0300

    Fix indentation bug

--- functions3.py
+++ functions3.py
@@ -30,28 +30,28 @@ def level_up_building(cursor, buildingid):
     global LEVEL_EFFECT
     cursor.execute("""
     select * from buildings where(buildingid=%s)""",(buildingid,))
-     building = cursor.fetchone()
-     city_name = building[1]
-     buildingname = building[2]
-     level = building[3]
-     cursor.execute("""
-     select buildtime from buildingtypes where(buildingname=%s)
-     """, (buildingname,))
+    building = cursor.fetchone()
+    city_name = building[1]
+    buildingname = building[2]
+    level = building[3]
+    cursor.execute("""
+    select buildtime from buildingtypes where(buildingname=%s)
+    """, (buildingname,))
 
-     base_time = cursor.fetchone()
-     base_time = base_time[0]
+    base_time = cursor.fetchone()
+    base_time = base_time[0]
 
-     new_level = int(level)+1
-     remaining_time = base_time + (base_time * (new_level)*LEVEL_EFFECT) // 100
-     cursor.execute("""
-     INSERT INTO PendingBuildings values(
-     %s,
-     %s,
-     %s,
-     %s,
-     %s
-     )
-     """,(buildingid, city_name, buildingname, new_level, remaining_time))
+    new_level = int(level)+1
+    remaining_time = base_time + (base_time * (new_level)*LEVEL_EFFECT) // 100
+    cursor.execute("""
+    INSERT INTO PendingBuildings values(
+    %s,
+    %s,
+    %s,
+    %s,
+    %s
+    )
+    """,(buildingid, city_name, buildingname, new_level, remaining_time))
 
 def updateBuildings(cursor):
     cursor.execute("""Select * from PendingBuildings """)
